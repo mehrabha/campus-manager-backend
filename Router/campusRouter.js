24f88316d6e18e7d4d9b2a3282a41791db510376
Sami <BlueBubbleBeam@outlook.com>
2019-08-10 00:24:10 -0400

    added sql queries to express routes:

--- Router/campusRouter.js
+++ Router/campusRouter.js
@@ -3,33 +3,6 @@ const router = express.Router();
 const Campus = require('../Database/Models/Campuses');
 const Student = require('../Database/Models/Students');
 
-let campuses = [
-	{
-		id: "0",
-		name: "hunter",
-		bio: "Cuny",
-		address: "123 Main St",
-		img: "https://png.pngtree.com/svg/20170616/22811e059c.svg"},
-	{
-		id: "1",
-		name: "baruch",
-		bio: "Cuny",
-		address: "456 Main St",
-		img: "https://png.pngtree.com/svg/20170616/22811e059c.svg"
-	},
-];
-
-function findById(id){ //helper function to search campuses by Id, IndexOf wasn't working properly
-	for(let i = 0; i < campuses.length; i++)
-	{
-		if(campuses[i].id == id)
-		{
-			return i;
-		}
-	}
-	return -1;
-}
-
 //GET localhost:3000/api/campuses
 router.get('/', (req,res) =>{
 	Campus.findAll({ include: [Student] }) //Eager Loading
@@ -37,38 +10,55 @@ router.get('/', (req,res) =>{
 	.catch(err => console.log(err))
 })
 
-//GET localhost:3000/api/campuses/1
+//GET localhost:3000/api/campuses/1 
 router.get('/:id', (req,res) =>{
 	Campus.findByPk(req.params.id)
-	.then(campus=> res.status(404).send(campus))
+	.then(campus=> res.status(200).send(campus))
 	.catch(err => console.log(err))
 })
 
 //POST localhost:3000/api/campuses //issues with duplicates
-router.post('/', (req,res)=>{
-	console.log(req.body);
-	campuses.push(req.body);
-	res.status(201).send(campuses);
+router.post('/', async (req,res)=>{
+	let data = req.body;
+	await Campus.create({
+		name: data.name,
+		bio: data.bio,
+		address: data.address,
+		img: data.img
+	});
+	console.log('User added');
+
+	Campus.findAll({ include: [Student] }) //Eager Loading
+	.then(campuses => res.status(201).json(campuses))
 })
 
 //PUT localhost:3000/api/campuses/1
-router.put('/:id', (req,res)=>{
-	let index = findById(req.params.id);
-	if(index != -1)
-	{
-		campuses[req.params.id] = req.body;
-		res.status(201).send(campuses);
-	}
-	else
-	{
-		res.status(402).send();
-	}
+router.put('/:id', async (req,res)=>{
+	let data = req.body;
+	await Campus.update(	//information to update it with
+		{
+			name: data.name,
+			bio: data.bio,
+			address: data.address,
+			img: data.img
+		},
+		{where: { id: req.params.id}}		//location in the database to update
+	)
+		
+	console.log("entry has been updated");
+	await Campus.findAll({ include: [Student] })
+	.then(campuses => res.status(201).json(campuses))
+	.catch(err => console.log(err))
 })
 
 //DELETE localhost:3000/api/campuses/1
-router.delete('/:id', (req,res)=>{
-	Campus.findByPk(req.params.id)
-	.then(campus => campus.destroy())
+router.delete('/:id', async(req,res)=>{
+	await Campus.destroy({ where: {id : req.params.id}});
+	console.log("entry DESTORYED");
+
+	await Campus.findAll({ include: [Student] })
+		.then(campuses => res.status(201).json(campuses))
+		.catch(err => console.log(err))
 })
 
 module.exports = router;
--- Router/studentRouter.js
+++ Router/studentRouter.js
@@ -1,134 +1,62 @@
 const express = require('express');
 const router1 = express.Router();
-
-let students = [
-    {
-        id: "0",
-        name: "Neil Tyson",
-        img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
-        gpa: 2.0,
-        college: 0
-    },
-    {
-        id: "1",
-        name: "Mike Tyson",
-        img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
-        gpa: 2.2,
-        college: 0
-     },
-     {
-        id: "2",
-        name: "Elon Musk",
-        img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
-        gpa: 2.7,
-        college: 0
-     },
-     {
-        id: "3",
-        name: "Elizabeth Ryler",
-        img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
-        gpa: 2.9,
-        college: 1
-    },
-    {
-        id: "4",
-        name: "Harold Kimp",
-        img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
-        gpa: 3.3,
-        college: 1
-    },
-    {
-        id: "5",
-        name: "Michelle Rubin",
-        img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
-        gpa: 1.0,
-        college: 1
-    },
-    {
-        id: "6",
-        name: "Kenneth Di",
-        img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
-        gpa: 0.1,
-        college: 1
-    },
-    {
-        id: "7",
-        name: "Lolita Lopez",
-        img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
-        gpa: 3.7,
-        college: 1
-    },
-    {
-        id: "8",
-        name: "Rain Man",
-        img: 'https://d29fhpw069ctt2.cloudfront.net/icon/image/49320/preview.svg',
-        gpa: 4.0,
-        college: 0
-    }
-];
-
-function findById(id){ //helper function to search students by Id, IndexOf wasn't working properly
-	for(let i = 0; i < students.length; i++)
-	{
-		if(students[i].id == id)
-		{
-			return i;
-		}
-	}
-	return -1;
-}
+const Campus = require('../Database/Models/Campuses');
+const Student = require('../Database/Models/Students');
 
 //GET localhost:3000/api/students
 router1.get('/', (req,res) =>{
-	res.status(200).send(students);
+	Student.findAll({ include: [Campus] }) //Eager Loading
+	.then(students => res.status(200).json(students))
+	.catch(err => console.log(err))
 })
 
 //GET localhost:3000/api/students/1
 router1.get('/:id', (req,res) =>{
-	if(findById(req.params.id) != -1)
-	{
-		res.status(200).send(students[req.params.id]);
-	}
-	else
-	{
-		console.log("Invalid ID");
-		res.status(404).send();
-	}
+	Student.findByPk(req.params.id)
+	.then(student=> res.status(200).send(student))
+	.catch(err => console.log(err))
 })
 
 //POST localhost:3000/api/students
-router1.post('/', (req,res)=>{
-	console.log(req.body);
-	students.push(req.body);
-	res.status(201).send(students);
+router1.post('/', async (req,res)=>{
+	let data = req.body;
+	await Student.create({
+		name: data.name,
+        img: data.img,
+        gpa: data.gpa
+	});
+	console.log('User added');
+
+	Student.findAll({ include: [Campus] }) //Eager Loading
+	.then(students => res.status(201).json(students))
 })
 
 //PUT localhost:3000/api/students/1
-router1.put('/:id', (req,res)=>{
-	let index = findById(req.params.id);
-	if(index != -1)
-	{
-		students[req.params.id] = req.body;
-		res.status(201).send(students);
-	}
-	else
-	{
-		res.status(402).send();
-	}
+router1.put('/:id', async(req,res)=>{
+	let data = req.body;
+	await Student.update(	//information to update it with
+		{
+			name: data.name,
+            img: data.img,
+            gpa: data.gpa
+		},
+		{where: { id: req.params.id}}		//location in the database to update
+	)
+		
+	console.log("entry has been updated");
+	await Student.findAll({ include: [Campus] })
+	.then(students => res.status(201).json(students))
+	.catch(err => console.log(err))
 })
 
 //DELETE localhost:3000/api/students/1
-router1.delete('/:id', (req,res)=>{
-	let index = findById(req.params.id);
-	if( index != -1)
-	{
-		students.splice(index,1);//removes one element starting from position index
-		res.status(201).send(students);
-	}
-	else{
-		console.log("Cannot delete selected element.");
-		res.status(402).send();
-	}
+router1.delete('/:id', async (req,res)=>{
+	await Student.destroy({ where: {id : req.params.id}});
+	console.log("entry DESTORYED");
+
+	await Student.findAll({ include: [Campus] })
+		.then(students => res.status(201).json(students))
+		.catch(err => console.log(err))
 })
 
 module.exports = router1;
