1347e2ef6108092a79ee9161caef7eb024454e78
Sami <BlueBubbleBeam@outlook.com>
2019-08-11 14:03:32 -0400

    .

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ db.sync({ force: true }).then(async () => {
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     });
 
